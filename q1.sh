dc3f403b2be12b390fbe86221b61eaaac3fae838
Manan Goel <manan.goel@research.iiit.ac.in>
2017-10-07 17:53:05 +0000

    Replace q1.sh

--- q1.sh
+++ q1.sh
@@ -12,10 +12,10 @@ for (( i = 1; i <= $counter; i++ ))
 do
   name=`cat name.txt | head -n $i | tail -1`
   size=`cat usage1.txt | head -n $i | tail -1`
-  if [ $size -lt $1 ]
+  if [ "$size" -lt "$1" ]
   then
     echo OK, $name, $size%
-  elif [ $size -lt $2 ]
+  elif [[ $size -lt "$2" && $size -gt "$1" ]]
   then
     echo Warning, $name, $size%
   else
