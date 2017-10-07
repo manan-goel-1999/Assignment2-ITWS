938a812eb3dd2c2e97b70703783ac3ea3c3aef66
Manan Goel <manan.goel@research.iiit.ac.in>
2017-10-07 22:53:38 +0530

    Question 1

---
+++ q1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+df -h | awk '{print $1}' > name.txt
+sed -i '/Filesystem/d' name.txt
+df -h | awk '{print $5}' > usage.txt
+sed -i '/Use%/d' usage.txt
+#usage.txt
+sed 's/%//' usage.txt > usage1.txt
+counter=`df -h | wc -l`
+counter=$(($counter-1))
+#echo $count
+for (( i = 1; i <= $counter; i++ ))
+do
+  name=`cat name.txt | head -n $i | tail -1`
+  size=`cat usage1.txt | head -n $i | tail -1`
+  if [ $size -lt $1 ]
+  then
+    echo OK, $name, $size%
+  elif [ $size -lt $2 ]
+  then
+    echo Warning, $name, $size%
+  else
+    echo Critical, $name, $size%
+  fi
+  #echo $name
+  #cho $size
+done
