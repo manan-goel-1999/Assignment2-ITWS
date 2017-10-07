b53732e70de28b50248f35624064eacdcab2d405
Manan Goel <manan.goel@research.iiit.ac.in>
2017-10-07 22:54:29 +0530

    Question 3

---
+++ q3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+find *.sh > files.txt
+echo "Enter String"
+read string
+tot="0"
+count=`cat files.txt | wc -l`
+#echo $count
+for (( i = 1; i <= $count; i++ ))
+do
+  file=`cat files.txt | head -n $i | tail -1`
+  #echo $file
+  counter=`grep -o "$string" $file | wc -l`
+  #echo $counter
+  if [[ $counter -gt "0" ]]
+  then
+    tot="1"
+    echo "$counter lines in $file"
+  fi
+done
+if [[ $tot -eq "0" ]]
+then
+  exit 1
+else
+  exit 0
+fi
