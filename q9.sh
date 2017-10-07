d90d5f6e0734c1d665993c1698042401df6d4c94
Manan Goel <manan.goel@research.iiit.ac.in>
2017-10-07 23:16:20 +0530

    Question 9

---
+++ q9.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Enter Name and Birthday"
+read name
+read bdate
+echo "$bdate"
+dateb=`echo $bdate | cut -b 6-10`
+datet=`date -I | cut -b 6-10`
+yearb=`echo $bdate | cut -b 1-4`
+yeart=`date -I | cut -b 1-4`
+if [[ $datet == $datet ]]
+then
+  echo "Happy Birthday, $name.You are $(($yeart-$yearb)) years old today!"
+fi
