3d47447cc8db3edcc96e5e6cbe53958a3adc2026
Manan Goel <manan.goel@research.iiit.ac.in>
2017-10-09 09:37:28 +0000

    Replace q6.sh

--- q6.sh
+++ q6.sh
@@ -2,12 +2,13 @@
 echo "Enter Monthly Salary"
 read salary
 salary=$((12*($salary)))
-a=0.3
+a=0.3000
 #echo "$a"
 if [[ salary -gt "300000" ]];
 then
-  tax=`echo $salary*$a | bc -l`
+  tax=$(bc <<< "scale=4; $salary*$a")
   echo "TAX PAYMENT REQUIRED : $tax"
 else
-  echo "NO​ ​TAX​ ​PAYMENT​ ​REQUIRED"
+  echo "NO TAX PAYMENT REQUIRED"
 fi
+
