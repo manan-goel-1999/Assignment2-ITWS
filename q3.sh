5aa1bcc7a33c5f3997e552c5078a82c87e554260
Manan Goel <manan.goel@research.iiit.ac.in>
2017-10-09 09:22:37 +0000

    Replace q3.sh

--- q3.sh
+++ q3.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-find *.sh > files.txt
+#find *.sh > files.txt
 echo "Enter String"
 read string
 tot="0"
-count=`cat files.txt | wc -l`
+count=$(echo $(find *.sh) | tr ' ' '\n'  | wc -l)
 #echo $count
 for (( i = 1; i <= $count; i++ ))
 do
-  file=`cat files.txt | head -n $i | tail -1`
-  #echo $file
-  counter=`grep -o "$string" $file | wc -l`
-  #echo $counter
+  file=$(echo $(find *.sh) | tr ' ' '\n' | head -n $i | tail -1)
+ # echo $file
+  counter=$(grep -o "$string" $file | wc -l)
+ # echo $counter
   if [[ $counter -gt "0" ]]
   then
     tot="1"
     echo "$counter lines in $file"
   fi
 done
-rm files.txt
+#rm files.txt
 if [[ $tot -eq "0" ]]
 then
   exit 1
