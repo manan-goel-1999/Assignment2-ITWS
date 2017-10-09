e6e124a8e67eb1daffb0b5ae5cd095bc9519aed5
Manan Goel <manan.goel@research.iiit.ac.in>
2017-10-09 10:12:24 +0000

    Replace time_ping.sh

--- time_ping.sh
+++ time_ping.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-ping -c "$1" www.google.com | while read endlooop; do echo "$(date): $endlooop"; done > pingfi.txt
-count=`cat pingfi.txt | wc -l`
+#ping -c "$1" www.google.com | while read endlooop; do echo "$(date): $endlooop"; done > pingfi.txt
+count=$(ping -c "$1" www.google.com | while read endloop; do echo "$(date): $endloop"; done | wc -l)
+#echo $count
 for (( i = 1; i <= $count; i++ ))
 do
-  pinger=`cat pingfi.txt | head -n $i | tail -n 1`
+  pinger=$(ping -c "$1" www.google.com | while read endloop; do echo "$(date): $endloop"; done | head -n $i | tail -n 1)
   echo "$(tput setaf $i) $pinger"
 done
-rm pingfi.txt
+
