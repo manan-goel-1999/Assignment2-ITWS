725b58cac9426e9597438e9ad25fdf77a6026f36
Manan Goel <manan.goel@research.iiit.ac.in>
2017-10-09 12:17:22 +0000

    Update q1.sh

--- q1.sh
+++ q1.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #df -h | awk '{print $1}' > name.txt
 #sed -i '/Filesystem/d' name.txt
-df -h | awk '{print $5}' > usage.txt
-sed -i '/Use%/d' usage.txt
+#df -h | awk '{print $5}' > usage.txt
+#sed -i '/Use%/d' usage.txt
 #usage.txt
-sed 's/%//' usage.txt > usage1.txt
+#sed 's/%//' usage.txt > usage1.txt
 counter=`df -h | wc -l`
 counter=$(($counter-1))
 #echo $count
 for (( i = 1; i <= $counter; i++ ))
 do
   name=`df -h | awk '{print $1}' | tail -n+2 | head -n $i | tail -1`
-  size=`cat usage1.txt | head -n $i | tail -1`
+  size=`df -h | awk '{print $5}' | tail -n+2 | tr -d '%' | head -n $i | tail -1`
   if [ "$size" -lt "$1" ]
   then
     echo OK, $name, $size%
@@ -24,6 +24,6 @@ do
   #echo $name
   #echo $size
   #rm name.txt
-  rm usage.txt
-  rm usage1.txt
+  #rm usage.txt
+  #rm usage1.txt
 done
