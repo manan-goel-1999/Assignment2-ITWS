9debcd07b29d44c77fbdb5595d1a2d3cbb14959b
Manan Goel <manan.goel@research.iiit.ac.in>
2017-10-12 03:38:22 +0000

    Update q9.sh

--- q9.sh
+++ q9.sh
@@ -7,7 +7,7 @@ dateb=`echo $bdate | cut -b 6-10`
 datet=`date -I | cut -b 6-10`
 yearb=`echo $bdate | cut -b 1-4`
 yeart=`date -I | cut -b 1-4`
-if [[ $datet == $datet ]]
+if [[ $dateb == $datet ]]
 then
   echo "Happy Birthday, $name.You are $(($yeart-$yearb)) years old today!"
 fi
